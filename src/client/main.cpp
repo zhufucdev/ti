1f9e0e0b85fcf7b6b1546c1ac157c175af99371a
zhufucdev <1095291434@qq.com>
2023-11-12 22:37:21 +0800

    :sparkles: tui

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,40 @@
-#include <client/protocol.hpp>
+#include <client/behavior.hpp>
+#include <log.hpp>
 
-int main (int argc, char *argv[]) {
-    std::cout<<"TI - The IM client version "<<ti::version<<std::endl;
+using ti::client::TiClient;
+
+static TiClient *client;
+
+void interrupt(int param);
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, interrupt);
+
+    std::cout << "TI - The IM client version " << ti::version << std::endl;
+    logD("Running a debugging version");
+
+    client = new TiClient("127.0.0.1", 6789, "ti_client.db");
+    client->start();
+
+    char cmd;
+    while (client->is_running()) {
+        std::cout << "ti> ";
+        std::cin >> cmd;
+
+        if (cmd == 'q') {
+            break;
+        }
+    }
+
+    if (client->is_running()) {
+        client->stop();
+    }
+    delete client;
     return 0;
+}
+
+void interrupt(int) {
+    if (client->is_running()) {
+        client->stop();
+    }
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,7 @@
 #include <csignal>
 #include <iostream>
 #include <server/behavior.hpp>
+#include <log.hpp>
 
 using ti::server::TiServer;
 
@@ -10,16 +11,19 @@ void interrupt(int param);
 
 int main(int argc, char const *argv[]) {
     std::cout<<"TI - The IM server version "<<ti::version<<std::endl;
-    
+    logD("Running a debug version");
+
     signal(SIGINT, interrupt);
 
     server = new TiServer("0.0.0.0", 6789, "ti_server.db");
-    std::cout<<"listen on "<<server->get_addr()<<":"<<server->get_port()<<std::endl;
+    std::cout<<"Listen on "<<server->get_addr()<<":"<<server->get_port()<<std::endl;
     server->start();
     delete server;
     return 0;
 }
 
-void interrupt(int param) {
-    server->stop();
+void interrupt(int) {
+    if (server->is_running()) {
+        server->stop();
+    }
 }
