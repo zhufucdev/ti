a79cdf98ad6b01099de9baeae424a488ae091771
zhufucdev <1095291434@qq.com>
2023-11-19 12:01:34 +0800

    :art: parallel test

--- src/test/orm.cc
+++ src/test/orm.cc
@@ -5,49 +5,51 @@
 
 class ServerOrmTest : public testing::Test {
   protected:
+    const std::string dbfile = nanoid::generate() + ".db";
     ti::server::ServerOrm *sorm{};
-    const std::string dbfile = "ti_server.db";
-    const std::string user_name = "Testificate Man";
-    const std::string user_id = "l1mITy-T1UBWsGeqLszsL";
-    const std::string bio = "I test a lot";
     const std::string group_name = "Sick Group";
     const std::string group_id = "C4E7tI86DrbVJdBMphpV0";
+    ti::User testificate_man{"l1mITy-T1UBWsGeqLszsL", "Testificate Man",
+                             "I test a lot"};
+    ti::User testificate_woman{"Z0RSddx7esE8lmT0fZ1Yc", "Testificate Woman",
+                               "I test a lot"};
 
     void SetUp() override { sorm = new ti::server::ServerOrm(dbfile); }
-    void TearDown() override { delete sorm; }
+    void TearDown() override {
+        delete sorm;
+        std::remove(dbfile.c_str());
+    }
 };
 
 TEST_F(ServerOrmTest, AddUser) {
-    sorm->pull();
-    sorm->add_user(new ti::User(user_id, user_name, bio), nanoid::generate());
-
+    sorm->add_user(&testificate_man, nanoid::generate());
     sorm->pull();
 
-    auto user = sorm->get_user(user_id);
+    auto user = sorm->get_user(testificate_man.get_id());
     ASSERT_NE(user, nullptr);
-    ASSERT_EQ(user->get_id(), user_id);
-    ASSERT_EQ(user->get_name(), user_name);
-    ASSERT_EQ(user->get_bio(), bio);
+    ASSERT_EQ(user->get_id(), testificate_man.get_id());
+    ASSERT_EQ(user->get_name(), testificate_man.get_name());
+    ASSERT_EQ(user->get_bio(), testificate_man.get_bio());
     delete user;
 }
 
 TEST_F(ServerOrmTest, GetEntities) {
+    sorm->add_user(&testificate_man, nanoid::generate());
     sorm->pull();
-    auto u = sorm->get_user(user_id);
+
+    auto u = sorm->get_user(testificate_man.get_id());
     ASSERT_NE(u, nullptr);
-    ASSERT_EQ(u->get_name(), user_name);
-    ASSERT_EQ(u->get_bio(), bio);
+    ASSERT_EQ(u->get_name(), testificate_man.get_name());
+    ASSERT_EQ(u->get_bio(), testificate_man.get_bio());
     delete u;
 }
 
 TEST_F(ServerOrmTest, AddGroup) {
-    sorm->pull();
-    auto u =
-        new ti::User(nanoid::generate(), "Testificate Woman", "I test a lot");
-    auto g =
-        new ti::Group(group_id, group_name,
-                      std::vector<ti::Entity *>{sorm->get_user(user_id), u});
-
+    sorm->add_user(&testificate_man, nanoid::generate());
+    sorm->add_user(&testificate_woman, nanoid::generate());
+    auto g = new ti::Group(
+        group_id, group_name,
+        std::vector<ti::Entity *>{&testificate_man, &testificate_woman});
     sorm->add_entity(g);
 
     sorm->pull();
@@ -57,6 +59,8 @@ TEST_F(ServerOrmTest, AddGroup) {
     ASSERT_NE(gb, nullptr);
     ASSERT_EQ(gb->get_name(), group_name);
     ASSERT_EQ(gb->get_members().size(), 2);
-    ASSERT_EQ(gb->get_members()[0]->get_id(), user_id);
-    ASSERT_EQ(gb->get_members()[1]->get_id(), u->get_id());
+    ASSERT_EQ(gb->get_members()[0]->get_id(), testificate_man.get_id());
+    ASSERT_EQ(gb->get_members()[1]->get_id(), testificate_woman.get_id());
+    delete g;
+    delete gb;
 }
