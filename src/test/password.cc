0acaf869619e675a54a1ff565acff320c69c8867
zhufucdev <1095291434@qq.com>
2023-11-21 07:50:46 +0800

    :white_check_mark: password test

---
+++ src/test/password.cc
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <nanoid.h>
+#include <ti_server.h>
+
+class PasswordTest : public testing::Test {
+  protected:
+    std::string dbfile = nanoid::generate() + ".db";
+    ti::server::ServerOrm *sorm = new ti::server::ServerOrm(dbfile);
+    const std::string passcode = "root";
+    ti::User *user = new ti::User(nanoid::generate(), "", "");
+
+    void SetUp() override { sorm->add_user(user, passcode); }
+    void TearDown() override {
+        delete sorm;
+        std::remove(dbfile.c_str());
+    }
+};
+
+TEST_F(PasswordTest, Hashing) {
+    ASSERT_TRUE(sorm->check_password(user->get_id(), passcode));
+    ASSERT_FALSE(sorm->check_password(user->get_id(), "toor"));
+}
