4b4d907e0eecf4c8fe939ca607a6ea7e62a824e0
zhufucdev <1095291434@qq.com>
2023-11-22 13:50:48 +0800

    :white_check_mark: TimeSqlTest

--- src/test/time.cc
+++ src/test/time.cc
@@ -8,3 +8,32 @@ TEST(Time, Stringify) {
 TEST(Time, Parse) {
     ASSERT_EQ(ti::parse_iso_time("2011-10-08T07:07:09Z"), 1318057629);
 }
+
+class TimeSqlTest : public testing::Test {
+  protected:
+    time_t now;
+    ti::orm::SqlDatabase *db;
+    std::string dbfile = "time_sql_test.db";
+    void SetUp() override {
+        std::time(&now);
+        ti::orm::SqlDatabase::initialize();
+        db = new ti::orm::SqlDatabase(dbfile);
+    }
+    void TearDown() override { delete db; }
+};
+
+TEST_F(TimeSqlTest, Sql) {
+    db->exec_sql(R"(CREATE TABLE "time"
+(
+    id    int primary key,
+    value datetime not null
+))");
+    auto t = db->prepare(R"(INSERT INTO "time"(value) VALUES (?))");
+    t->bind_text(0, ti::to_iso_time(now));
+    t->begin();
+    delete t;
+
+    t = db->prepare(R"(SELECT value FROM "time")");
+    auto row = *(t->begin());
+    ASSERT_EQ(ti::parse_iso_time(row.get_text(0)), now);
+}
