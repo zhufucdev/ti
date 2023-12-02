17755f8ae55a9d80916ece303eebcc857d7421d2
zhufucdev <1095291434@qq.com>
2023-12-02 11:41:05 +0800

    :white_check_mark: AddContact

--- src/test/orm.cc
+++ src/test/orm.cc
@@ -7,12 +7,13 @@ class ServerOrmTest : public testing::Test {
   protected:
     const std::string dbfile = nanoid::generate() + ".db";
     ti::server::ServerOrm *sorm{};
-    const std::string group_name = "Sick Group";
-    const std::string group_id = "C4E7tI86DrbVJdBMphpV0";
     ti::User testificate_man{"l1mITy-T1UBWsGeqLszsL", "Testificate Man",
                              "I test a lot", 0};
     ti::User testificate_woman{"Z0RSddx7esE8lmT0fZ1Yc", "Testificate Woman",
                                "I test a lot", 0};
+    ti::Group group{
+        "C4E7tI86DrbVJdBMphpV0", "Sick Group",
+        std::vector<ti::Entity *>{&testificate_man, &testificate_woman}};
 
     void SetUp() override { sorm = new ti::server::ServerOrm(dbfile); }
     void TearDown() override {
@@ -22,7 +23,7 @@ class ServerOrmTest : public testing::Test {
 };
 
 TEST_F(ServerOrmTest, AddUser) {
-    sorm->add_user(&testificate_man, nanoid::generate());
+    sorm->add_entity(&testificate_man);
     sorm->pull();
 
     auto user = sorm->get_user(testificate_man.get_id());
@@ -34,7 +35,7 @@ TEST_F(ServerOrmTest, AddUser) {
 }
 
 TEST_F(ServerOrmTest, GetEntities) {
-    sorm->add_user(&testificate_man, nanoid::generate());
+    sorm->add_entity(&testificate_man);
     sorm->pull();
 
     auto u = sorm->get_user(testificate_man.get_id());
@@ -45,31 +46,27 @@ TEST_F(ServerOrmTest, GetEntities) {
 }
 
 TEST_F(ServerOrmTest, AddGroup) {
-    sorm->add_user(&testificate_man, nanoid::generate());
-    sorm->add_user(&testificate_woman, nanoid::generate());
-    auto g = new ti::Group(
-        group_id, group_name,
-        std::vector<ti::Entity *>{&testificate_man, &testificate_woman});
-    sorm->add_entity(g);
+    sorm->add_entity(&testificate_man);
+    sorm->add_entity(&testificate_woman);
+    sorm->add_entity(&group);
 
     sorm->pull();
-    auto e = sorm->get_entity(group_id);
+    auto e = sorm->get_entity(group.get_id());
     ASSERT_NE(e, nullptr);
     auto gb = dynamic_cast<ti::Group *>(e);
     ASSERT_NE(gb, nullptr);
-    ASSERT_EQ(gb->get_name(), group_name);
+    ASSERT_EQ(gb->get_name(), group.get_name());
     ASSERT_EQ(gb->get_members().size(), 2);
     ASSERT_EQ(gb->get_members()[0]->get_id(), testificate_man.get_id());
     ASSERT_EQ(gb->get_members()[1]->get_id(), testificate_woman.get_id());
-    delete g;
     delete gb;
 }
 
 TEST_F(ServerOrmTest, AddMessage) {
     time_t now;
     time(&now);
-    sorm->add_user(&testificate_man, nanoid::generate());
-    sorm->add_user(&testificate_woman, nanoid::generate());
+    sorm->add_entity(&testificate_man);
+    sorm->add_entity(&testificate_woman);
     std::vector<ti::Frame *> frames = {
         new ti::TextFrame(nanoid::generate(), "Hi I'm John Cena"),
         new ti::TextFrame(nanoid::generate(), "I like bing chilling")};
@@ -82,4 +79,17 @@ TEST_F(ServerOrmTest, AddMessage) {
     ASSERT_EQ(m->get_time(), now);
     ASSERT_EQ(m->get_frames().size(), frames.size());
     ASSERT_EQ(m->get_frames()[0]->get_id(), frames[0]->get_id());
+}
+
+TEST_F(ServerOrmTest, AddContact) {
+    sorm->add_entity(&testificate_man);
+    sorm->add_entity(&testificate_woman);
+    sorm->add_entity(&group);
+    sorm->add_contact(&testificate_man, &testificate_woman);
+    sorm->add_contact(&testificate_man, &group);
+    sorm->pull();
+
+    auto contacts = sorm->get_contacts(&testificate_man);
+    ASSERT_EQ(contacts[0]->get_id(), testificate_woman.get_id());
+    ASSERT_EQ(contacts[1]->get_id(), group.get_id());
 }
