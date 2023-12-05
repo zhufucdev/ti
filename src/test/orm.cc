5999313f2dfedaaf33ccdaf12caa893a8ccd3f46
zhufucdev <1095291434@qq.com>
2023-12-05 19:32:31 +0800

    :white_check_mark: make it work with the destructor

--- src/test/orm.cc
+++ src/test/orm.cc
@@ -23,7 +23,7 @@ class ServerOrmTest : public testing::Test {
 };
 
 TEST_F(ServerOrmTest, AddUser) {
-    sorm->add_entity(&testificate_man);
+    sorm->add_entity(new ti::User(testificate_man));
     sorm->pull();
 
     auto user = sorm->get_user(testificate_man.get_id());
@@ -31,24 +31,22 @@ TEST_F(ServerOrmTest, AddUser) {
     ASSERT_EQ(user->get_id(), testificate_man.get_id());
     ASSERT_EQ(user->get_name(), testificate_man.get_name());
     ASSERT_EQ(user->get_bio(), testificate_man.get_bio());
-    delete user;
 }
 
 TEST_F(ServerOrmTest, GetEntities) {
-    sorm->add_entity(&testificate_man);
+    sorm->add_entity(new ti::User(testificate_man));
     sorm->pull();
 
     auto u = sorm->get_user(testificate_man.get_id());
     ASSERT_NE(u, nullptr);
     ASSERT_EQ(u->get_name(), testificate_man.get_name());
     ASSERT_EQ(u->get_bio(), testificate_man.get_bio());
-    delete u;
 }
 
 TEST_F(ServerOrmTest, AddGroup) {
-    sorm->add_entity(&testificate_man);
-    sorm->add_entity(&testificate_woman);
-    sorm->add_entity(&group);
+    sorm->add_entity(new ti::User(testificate_man));
+    sorm->add_entity(new ti::User(testificate_woman));
+    sorm->add_entity(new ti::Group(group));
 
     sorm->pull();
     auto e = sorm->get_entity(group.get_id());
@@ -59,40 +57,47 @@ TEST_F(ServerOrmTest, AddGroup) {
     ASSERT_EQ(gb->get_members().size(), 2);
     ASSERT_EQ(gb->get_members()[0]->get_id(), testificate_man.get_id());
     ASSERT_EQ(gb->get_members()[1]->get_id(), testificate_woman.get_id());
-    delete gb;
 }
 
 TEST_F(ServerOrmTest, AddMessage) {
     time_t now;
     time(&now);
-    sorm->add_entity(&testificate_man);
-    sorm->add_entity(&testificate_woman);
-    std::vector<ti::Frame *> frames = {
-        new ti::TextFrame(nanoid::generate(), "Hi I'm John Cena"),
-        new ti::TextFrame(nanoid::generate(), "I like bing chilling"),
-        new ti::TextFrame(nanoid::generate(), nanoid::generate(1 << 12))};
-    auto msg = new ti::Message(nanoid::generate(), frames, now,
-                               &testificate_man, &testificate_woman, nullptr);
-    sorm->add_message(msg);
+    auto tm = new ti::User(testificate_man),
+         tw = new ti::User(testificate_woman);
+    sorm->add_entity(tm);
+    sorm->add_entity(tw);
+    ti::TextFrame tfs[] = {
+        ti::TextFrame(nanoid::generate(), "Hi I'm John Cena"),
+        ti::TextFrame(nanoid::generate(), "I like bing chilling"),
+        ti::TextFrame(nanoid::generate(), nanoid::generate(1 << 12))};
+    std::vector<ti::Frame *> frames = {new ti::TextFrame(tfs[0]),
+                                       new ti::TextFrame(tfs[1]),
+                                       new ti::TextFrame(tfs[2])};
+    auto msg = ti::Message(nanoid::generate(), frames, now, tm, tw, nullptr);
+    sorm->add_message(new ti::Message(msg));
 
     sorm->pull();
-    auto m = sorm->get_message(msg->get_id());
+    auto m = sorm->get_message(msg.get_id());
     ASSERT_EQ(m->get_time(), now);
     ASSERT_EQ(m->get_frames().size(), frames.size());
-    ASSERT_EQ(m->get_frames()[0]->get_id(), frames[0]->get_id());
-    ASSERT_EQ(m->get_frames()[0]->to_string(), frames[0]->to_string());
-    ASSERT_EQ(m->get_frames()[2]->to_string(), frames[2]->to_string());
+    ASSERT_EQ(m->get_frames()[0]->get_id(), tfs[0].get_id());
+    ASSERT_EQ(m->get_frames()[0]->to_string(), tfs[0].to_string());
+    ASSERT_EQ(m->get_frames()[2]->to_string(), tfs[2].to_string());
 }
 
 TEST_F(ServerOrmTest, AddContact) {
-    sorm->add_entity(&testificate_man);
-    sorm->add_entity(&testificate_woman);
-    sorm->add_entity(&group);
-    sorm->add_contact(&testificate_man, &testificate_woman);
-    sorm->add_contact(&testificate_man, &group);
+    auto tm = new ti::User(testificate_man),
+         tw = new ti::User(testificate_woman);
+    auto g = new ti::Group(group);
+    sorm->add_entity(tm);
+    sorm->add_entity(tw);
+    sorm->add_entity(g);
+    sorm->add_contact(tm, tw);
+    sorm->add_contact(tm, g);
     sorm->pull();
 
-    auto contacts = sorm->get_contacts(&testificate_man);
+    auto contacts =
+        sorm->get_contacts(sorm->get_user(testificate_man.get_id()));
     ASSERT_EQ(contacts[0]->get_id(), testificate_woman.get_id());
     ASSERT_EQ(contacts[1]->get_id(), group.get_id());
 }
